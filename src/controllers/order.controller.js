9e439add18ede086fd0b38e23c84659c7e3f09bc
perviin <pervin.eren1310@gmail.com>
2025-11-14 02:23:19 +0100

    order controller - services

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,13 +1,11 @@
-const { Order } = require('../models/postgres/Order');
-const Game = require('../models/postgres/Game');
-const ActivityLog = require('../models/mongo/ActivityLog');
+const orderService = require('../services/order.service');
 const { HTTP_STATUS } = require('../config/constants');
 const { asyncHandler } = require('../middlewares/errorHandler.middleware');
 
 class OrderController {
   getUserOrders = asyncHandler(async (req, res) => {
     const userId = req.user.id;
-    const orders = await Order.findByUserId(userId);
+    const orders = await orderService.getUserOrders(userId);
     res.status(HTTP_STATUS.OK).json({ success: true, data: orders });
   });
 
@@ -15,35 +13,28 @@ class OrderController {
     const { gameId } = req.body;
     const userId = req.user.id;
 
-    const game = await Game.findById(gameId);
-    if (!game) {
-      return res.status(HTTP_STATUS.NOT_FOUND).json({
-        success: false,
-        message: 'Jeu introuvable'
+    try {
+      const order = await orderService.createOrder(userId, gameId);
+      res.status(HTTP_STATUS.CREATED).json({
+        success: true,
+        message: 'Jeu acheté',
+        data: order
       });
+    } catch (err) {
+      if (err.message === 'GAME_NOT_FOUND') {
+        return res.status(HTTP_STATUS.NOT_FOUND).json({
+          success: false,
+          message: 'Jeu introuvable'
+        });
+      }
+      if (err.message === 'ALREADY_OWNED') {
+        return res.status(HTTP_STATUS.CONFLICT).json({
+          success: false,
+          message: 'Vous possédez déjà ce jeu'
+        });
+      }
+      throw err;
     }
-
-    const alreadyOwned = await Order.userOwnsGame(userId, gameId);
-    if (alreadyOwned) {
-      return res.status(HTTP_STATUS.CONFLICT).json({
-        success: false,
-        message: 'Vous possédez déjà ce jeu'
-      });
-    }
-
-    const order = await Order.create(userId, gameId, game.price);
-
-    await ActivityLog.create({
-      userId,
-      action: 'GAME_PURCHASE',
-      details: { gameId, gameTitle: game.title, pricePaid: game.price }
-    });
-
-    res.status(HTTP_STATUS.CREATED).json({
-      success: true,
-      message: 'Jeu acheté',
-      data: order
-    });
   });
 }
 
